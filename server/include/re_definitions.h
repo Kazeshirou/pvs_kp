7d1727b82f8043b8cfb8275384ae5ebc0b87c60c
Natalie Zharova <zharntl@yandex.ru>
2021-02-11 18:41:45 +0300

    feature: обновлён скрипт для добавления регулярок в отчёт

--- server/include/re_definitions.h
+++ server/include/re_definitions.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// clang-format off
 #define RE_CRLF                 "( ?\\x0D\\x0A)"
 #define RE_LET_DIG              "[a-zA-Z0-9]"
 #define RE_LDH_STR              "([a-zA-Z0-9\\-]*" RE_LET_DIG ")"
@@ -12,50 +13,25 @@
 #define RE_IPV6_HEX_WITH_COLON  "(:" RE_IPV6_HEX ")"
 #define RE_IPV6_FULL            "(" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{7})"
 #define RE_IPV6_COMP0           "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,5})?::)"
-#define RE_IPV6_COMP1 \
-    "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,4})?::" RE_IPV6_HEX ")"
-#define RE_IPV6_COMP2                              \
-    "((" RE_IPV6_HEX        RE_IPV6_HEX_WITH_COLON \
-    "{0,3})?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON ")"
-#define RE_IPV6_COMP3                              \
-    "((" RE_IPV6_HEX        RE_IPV6_HEX_WITH_COLON \
-    "{0,2})?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{2})"
-#define RE_IPV6_COMP4                          \
-    "((" RE_IPV6_HEX    RE_IPV6_HEX_WITH_COLON \
-    "?)?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{3})"
-#define RE_IPV6_COMP5 \
-    "(" RE_IPV6_HEX "?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{4})"
+#define RE_IPV6_COMP1 "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,4})?::" RE_IPV6_HEX ")"
+#define RE_IPV6_COMP2 "((" RE_IPV6_HEX        RE_IPV6_HEX_WITH_COLON "{0,3})?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON ")"
+#define RE_IPV6_COMP3 "((" RE_IPV6_HEX        RE_IPV6_HEX_WITH_COLON "{0,2})?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{2})"
+#define RE_IPV6_COMP4 "((" RE_IPV6_HEX    RE_IPV6_HEX_WITH_COLON "?)?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{3})"
+#define RE_IPV6_COMP5 "(" RE_IPV6_HEX "?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{4})"
 #define RE_IPV6_COMP6 "(::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{5})"
-#define RE_IPV6_COMP                                                        \
-    "(" RE_IPV6_COMP0 "|" RE_IPV6_COMP1 "|" RE_IPV6_COMP2 "|" RE_IPV6_COMP3 \
-    "|" RE_IPV6_COMP4 "|" RE_IPV6_COMP5 "|" RE_IPV6_COMP6 ")"
-#define RE_IPV6V4_FULL \
-    "(" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{5}:" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP0                     \
-    "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON \
-    "{0,3})?::" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP1                                             \
-    "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,2})?::" RE_IPV6_HEX \
-    ":" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP2                        \
-    "((" RE_IPV6_HEX    RE_IPV6_HEX_WITH_COLON \
-    "?)?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON ":" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP3                                      \
-    "(" RE_IPV6_HEX "?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON \
-    "{2}:" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP4 \
-    "(::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{3}:" RE_IPV4_ADDRESS_LITERAL ")"
-#define RE_IPV6V4_COMP                                          \
-    "(" RE_IPV6V4_COMP0 "|" RE_IPV6V4_COMP1 "|" RE_IPV6V4_COMP2 \
-    "|" RE_IPV6V4_COMP3 "|" RE_IPV6V4_COMP4 ")"
-#define RE_IPV6_ADDR \
-    "(" RE_IPV6_FULL "|" RE_IPV6_COMP "|" RE_IPV6V4_FULL "|" RE_IPV6V4_COMP ")"
+#define RE_IPV6_COMP  "(" RE_IPV6_COMP0 "|" RE_IPV6_COMP1 "|" RE_IPV6_COMP2 "|" RE_IPV6_COMP3 "|" RE_IPV6_COMP4 "|" RE_IPV6_COMP5 "|" RE_IPV6_COMP6 ")"
+#define RE_IPV6V4_FULL "(" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{5}:" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP0 "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,3})?::" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP1 "((" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{0,2})?::" RE_IPV6_HEX ":" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP2 "((" RE_IPV6_HEX    RE_IPV6_HEX_WITH_COLON "?)?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON ":" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP3 "(" RE_IPV6_HEX "?::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{2}:" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP4 "(::" RE_IPV6_HEX RE_IPV6_HEX_WITH_COLON "{3}:" RE_IPV4_ADDRESS_LITERAL ")"
+#define RE_IPV6V4_COMP  "(" RE_IPV6V4_COMP0 "|" RE_IPV6V4_COMP1 "|" RE_IPV6V4_COMP2 "|" RE_IPV6V4_COMP3 "|" RE_IPV6V4_COMP4 ")"
+#define RE_IPV6_ADDR "(" RE_IPV6_FULL "|" RE_IPV6_COMP "|" RE_IPV6V4_FULL "|" RE_IPV6V4_COMP ")"
 #define RE_IPV6_ADDRESS_LITERAL    "(IPv6:" RE_IPV6_ADDR ")"
 #define RE_DCONTENT                "([\\x21-\\x5A\\x5E-\\x7C])"
 #define RE_GENERAL_ADDRESS_LITERAL "(" RE_LDH_STR ":" RE_DCONTENT "+)"
-#define RE_ADDRES_LITERAL                                       \
-    "(\\[(" RE_IPV4_ADDRESS_LITERAL "|" RE_IPV6_ADDRESS_LITERAL \
-    "|" RE_GENERAL_ADDRESS_LITERAL ")])"
+#define RE_ADDRES_LITERAL "(\\[(" RE_IPV4_ADDRESS_LITERAL "|" RE_IPV6_ADDRESS_LITERAL "|" RE_GENERAL_ADDRESS_LITERAL ")])"
 #define RE_AT_DOMAIN       "(@" RE_DOMAIN ")"
 #define RE_A_D_L           "(" RE_AT_DOMAIN "(," RE_AT_DOMAIN ")?)"
 #define RE_ATEXT           "[a-zA-Z0-9!#$%&'*+-=/?^_`{}|~]"
@@ -81,12 +57,11 @@
 
 #define RE_HELO "^helo " RE_DOMAIN RE_CRLF
 #define RE_EHLO "^ehlo (" RE_DOMAIN "|" RE_ADDRES_LITERAL ")" RE_CRLF
-#define RE_MAIL \
-    "^mail from:" RE_REVERSE_PATH "( " RE_MAIL_PARAMETERS ")?" RE_CRLF
-#define RE_RCPT                                                              \
-    "^rcpt to:((<Postmaster@" RE_DOMAIN ">)|(<Postmaster>)|" RE_FORWARD_PATH \
-    ")( " RE_RCPT_PARAMETERS ")?" RE_CRLF
+#define RE_MAIL "^mail from:" RE_REVERSE_PATH "( " RE_MAIL_PARAMETERS ")?" RE_CRLF
+#define RE_RCPT "^rcpt to:((<Postmaster@" RE_DOMAIN ">)|(<Postmaster>)|" RE_FORWARD_PATH ")( " RE_RCPT_PARAMETERS ")?" RE_CRLF
 #define RE_DATA "^data" RE_CRLF
 #define RE_RSET "^rset" RE_CRLF
 #define RE_QUIT "^quit" RE_CRLF
 #define RE_VRFY "^vrfy " RE_STRING RE_CRLF
+
+// clang-format on
