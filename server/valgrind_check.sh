40de3c79e0102dcdf98c58a955f5e4efc26d01fc
Natalie Zharova <zharntl@yandex.ru>
2021-02-16 15:44:48 +0300

    update: valgrind_check.sh

--- server/valgrind_check.sh
+++ server/valgrind_check.sh
@@ -5,6 +5,12 @@ valgrind --leak-check=full \
          --track-origins=yes \
          --verbose \
          --log-file=valgrind-out.txt \
-         ./server.elf -l/tmp/mysmtp -c/tmp/mysmtp_clien
+         --error-exitcode=1 \
+         ./server.elf -l/tmp/mysmtp -c/tmp/mysmtp_client 2> /dev/null
 
-         
+if [ $? -eq 0 ]
+then
+  exit 0
+else
+  exit 1
+fi
